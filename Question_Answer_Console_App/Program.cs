cfabe7c40807ed32342f8725265f48ebe709c64e
Michael Brian Puckett, II <michael_puckett_ii@live.com>
2018-06-21 17:07:03 -0400

    Compare to Identitcal Tables for Changed Value in C# Question

--- Question_Answer_Console_App/Program.cs
+++ Question_Answer_Console_App/Program.cs
@@ -1,57 +1,102 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+
 namespace Question_Answer_Console_App
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            // A reference to IEnumerable<T>
-            // Behaves much like a function when used in a loop.
-            var items = GetItems<object>();
+            var idCounter = 0;
+            var existingStateModels = new List<StateModel>()
+            {
+                new StateModel() { Id = ++idCounter, State = "AL", IsActive = null },
+                new StateModel() { Id = ++idCounter, State = "AK", IsActive = null },
+                new StateModel() { Id = ++idCounter, State = "AZ", IsActive = null },
+                new StateModel() { Id = ++idCounter, State = "AR", IsActive = null },
+                new StateModel() { Id = ++idCounter, State = "CA", IsActive = null },
+                new StateModel() { Id = ++idCounter, State = "CO", IsActive = null },
+                new StateModel() { Id = ++idCounter, State = "CT", IsActive = null },
+                new StateModel() { Id = ++idCounter, State = "DE", IsActive = null },
+                new StateModel() { Id = ++idCounter, State = "FL", IsActive = null },
+                new StateModel() { Id = ++idCounter, State = "GA", IsActive = true },
+                new StateModel() { Id = ++idCounter, State = "HI", IsActive = true },
+                new StateModel() { Id = ++idCounter, State = "ID", IsActive = true },
+                new StateModel() { Id = ++idCounter, State = "IL", IsActive = true },
+                new StateModel() { Id = ++idCounter, State = "IN", IsActive = true },
+                new StateModel() { Id = ++idCounter, State = "IA", IsActive = true },
+                new StateModel() { Id = ++idCounter, State = "KS", IsActive = null },
+                new StateModel() { Id = ++idCounter, State = "KY", IsActive = null },
+                new StateModel() { Id = ++idCounter, State = "LA", IsActive = null },
+                new StateModel() { Id = ++idCounter, State = "ME", IsActive = null },
+                new StateModel() { Id = ++idCounter, State = "MD", IsActive = null }
+            };
 
-            // For each works on IEnumerable types and internally calls the GetEnumerator() method.
-            // GetEnumerator() returns IEnumerator which has 1 property and 2 methods :: Current, MoveNext(), and Reset()
-            // It basically calls GetEnumerator() and then MoveNext() and Current
-            foreach (var item in items)
-                Console.WriteLine(item);
+            idCounter = 0;
+            var newStateModels = new List<StateModel>()
+            {
+                new StateModel() { Id = ++idCounter, State = "AL", IsActive = null },
+                new StateModel() { Id = ++idCounter, State = "AK", IsActive = null },
+                new StateModel() { Id = ++idCounter, State = "AZ", IsActive = null },
+                new StateModel() { Id = ++idCounter, State = "AR", IsActive = null },
+                new StateModel() { Id = ++idCounter, State = "CA", IsActive = null },
+                new StateModel() { Id = ++idCounter, State = "CO", IsActive = null },
+                new StateModel() { Id = ++idCounter, State = "CT", IsActive = null },
+                new StateModel() { Id = ++idCounter, State = "DE", IsActive = null },
+                new StateModel() { Id = ++idCounter, State = "FL", IsActive = null },
+                new StateModel() { Id = ++idCounter, State = "GA", IsActive = true },
+                new StateModel() { Id = ++idCounter, State = "HI", IsActive = false },
+                new StateModel() { Id = ++idCounter, State = "ID", IsActive = true },
+                new StateModel() { Id = ++idCounter, State = "IL", IsActive = true },
+                new StateModel() { Id = ++idCounter, State = "IN", IsActive = false },
+                new StateModel() { Id = ++idCounter, State = "IA", IsActive = true },
+                new StateModel() { Id = ++idCounter, State = "KS", IsActive = null },
+                new StateModel() { Id = ++idCounter, State = "KY", IsActive = null },
+                new StateModel() { Id = ++idCounter, State = "LA", IsActive = null },
+                new StateModel() { Id = ++idCounter, State = "ME", IsActive = null },
+                new StateModel() { Id = ++idCounter, State = "MD", IsActive = true }
+            };
 
-            //Here's an example of the loop above written manually just to help you visualize it.
-            var enumerator = items.GetEnumerator();
-            while (enumerator.MoveNext())
-                Console.WriteLine(enumerator.Current);
+            var updatedStateModels = from existingModel in existingStateModels
+                                     join newModel in newStateModels
+                                     on existingModel.Id equals newModel.Id
+                                     where existingModel.IsActive != newModel.IsActive
+                                     select newModel;
 
-            //Finally; we can't write syntax but here's a method (seen below) that's much like the foreach syntax.
-            ForEach(items, item => Console.WriteLine(item));
+            foreach (var stateModel in updatedStateModels)
+            {
+                Console.WriteLine($"Id: {stateModel.Id}\tState: {stateModel.State}\tIsActive: {GetIsActiveValue(stateModel.IsActive)}");
+            }
 
             Console.Read();
         }
 
-        private static void ForEach<T>(IEnumerable<T> items, Action<T> action)
+        private static string GetIsActiveValue(bool? isActive)
         {
-            IEnumerator<T> enumerator = items.GetEnumerator(); 
-            iterateItems();
-            
-            void iterateItems()
+            switch (isActive)
             {
-                // Notice that it's not actually until we get HERE calling MoveNext() that the yield return Activator.CreateInstance<T>() is actually called in our application.
-                // And even now we don't have a reference to it ourselves; it's just placed into the Current property of the IEnumerator<T> type.
-                var hasAnotherItem = enumerator.MoveNext();
-                if (hasAnotherItem)
-                {
-                    // HERE is where we finally gain our own reference to the instantiated object for this example.
-                    T currentItem = enumerator.Current;
-                    action.Invoke(currentItem);
-                    iterateItems();
-                }
+                case true: return "1";
+                case false: return "0";
+                case null:
+                default: return "NULL";
             }
         }
 
-        private static IEnumerable<T> GetItems<T>()
-        {
-            yield return Activator.CreateInstance<T>();
-        }
+        /* 
+         * Outputs:
+         * Id: 11  State: HI       IsActive: 0
+         * Id: 14  State: IN       IsActive: 0
+         * Id: 20  State: MD       IsActive: 1 
+         */
     }
-    // Outputs: 
-    // System.Object
+
+    public class StateModel
+    {
+        public int Id { get; set; }
+        public string State { get; set; }
+        public bool? IsActive { get; set; }
+    }
 }
+ 
