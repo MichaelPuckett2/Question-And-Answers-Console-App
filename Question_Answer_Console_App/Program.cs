624adce351a4e6be0db6bf7c690ea1423ee14399
Michael Brian Puckett, II <michael_puckett_ii@live.com>
2018-10-14 11:47:05 -0400

    Explicit Interface implementations

--- Question_Answer_Console_App/Program.cs
+++ Question_Answer_Console_App/Program.cs
@@ -1,102 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Question_Answer_Console_App
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-            var idCounter = 0;
-            var existingStateModels = new List<StateModel>()
-            {
-                new StateModel() { Id = ++idCounter, State = "AL", IsActive = null },
-                new StateModel() { Id = ++idCounter, State = "AK", IsActive = null },
-                new StateModel() { Id = ++idCounter, State = "AZ", IsActive = null },
-                new StateModel() { Id = ++idCounter, State = "AR", IsActive = null },
-                new StateModel() { Id = ++idCounter, State = "CA", IsActive = null },
-                new StateModel() { Id = ++idCounter, State = "CO", IsActive = null },
-                new StateModel() { Id = ++idCounter, State = "CT", IsActive = null },
-                new StateModel() { Id = ++idCounter, State = "DE", IsActive = null },
-                new StateModel() { Id = ++idCounter, State = "FL", IsActive = null },
-                new StateModel() { Id = ++idCounter, State = "GA", IsActive = true },
-                new StateModel() { Id = ++idCounter, State = "HI", IsActive = true },
-                new StateModel() { Id = ++idCounter, State = "ID", IsActive = true },
-                new StateModel() { Id = ++idCounter, State = "IL", IsActive = true },
-                new StateModel() { Id = ++idCounter, State = "IN", IsActive = true },
-                new StateModel() { Id = ++idCounter, State = "IA", IsActive = true },
-                new StateModel() { Id = ++idCounter, State = "KS", IsActive = null },
-                new StateModel() { Id = ++idCounter, State = "KY", IsActive = null },
-                new StateModel() { Id = ++idCounter, State = "LA", IsActive = null },
-                new StateModel() { Id = ++idCounter, State = "ME", IsActive = null },
-                new StateModel() { Id = ++idCounter, State = "MD", IsActive = null }
-            };
+            var test = new Test();
 
-            idCounter = 0;
-            var newStateModels = new List<StateModel>()
-            {
-                new StateModel() { Id = ++idCounter, State = "AL", IsActive = null },
-                new StateModel() { Id = ++idCounter, State = "AK", IsActive = null },
-                new StateModel() { Id = ++idCounter, State = "AZ", IsActive = null },
-                new StateModel() { Id = ++idCounter, State = "AR", IsActive = null },
-                new StateModel() { Id = ++idCounter, State = "CA", IsActive = null },
-                new StateModel() { Id = ++idCounter, State = "CO", IsActive = null },
-                new StateModel() { Id = ++idCounter, State = "CT", IsActive = null },
-                new StateModel() { Id = ++idCounter, State = "DE", IsActive = null },
-                new StateModel() { Id = ++idCounter, State = "FL", IsActive = null },
-                new StateModel() { Id = ++idCounter, State = "GA", IsActive = true },
-                new StateModel() { Id = ++idCounter, State = "HI", IsActive = false },
-                new StateModel() { Id = ++idCounter, State = "ID", IsActive = true },
-                new StateModel() { Id = ++idCounter, State = "IL", IsActive = true },
-                new StateModel() { Id = ++idCounter, State = "IN", IsActive = false },
-                new StateModel() { Id = ++idCounter, State = "IA", IsActive = true },
-                new StateModel() { Id = ++idCounter, State = "KS", IsActive = null },
-                new StateModel() { Id = ++idCounter, State = "KY", IsActive = null },
-                new StateModel() { Id = ++idCounter, State = "LA", IsActive = null },
-                new StateModel() { Id = ++idCounter, State = "ME", IsActive = null },
-                new StateModel() { Id = ++idCounter, State = "MD", IsActive = true }
-            };
+            test.Print();
+            ((ITestA)test).Print();
+            ((ITestB)test).Print();
 
-            var updatedStateModels = from existingModel in existingStateModels
-                                     join newModel in newStateModels
-                                     on existingModel.Id equals newModel.Id
-                                     where existingModel.IsActive != newModel.IsActive
-                                     select newModel;
-
-            foreach (var stateModel in updatedStateModels)
-            {
-                Console.WriteLine($"Id: {stateModel.Id}\tState: {stateModel.State}\tIsActive: {GetIsActiveValue(stateModel.IsActive)}");
-            }
-
-            Console.Read();
+            Console.ReadKey();
         }
 
-        private static string GetIsActiveValue(bool? isActive)
-        {
-            switch (isActive)
-            {
-                case true: return "1";
-                case false: return "0";
-                case null:
-                default: return "NULL";
-            }
-        }
-
-        /* 
-         * Outputs:
-         * Id: 11  State: HI       IsActive: 0
-         * Id: 14  State: IN       IsActive: 0
-         * Id: 20  State: MD       IsActive: 1 
-         */
+        // OUTPUT
+        // Test
+        // ITestA
+        // ITestB
     }
 
-    public class StateModel
+    public interface ITestA
     {
-        public int Id { get; set; }
-        public string State { get; set; }
-        public bool? IsActive { get; set; }
+        void Print();
+    }
+
+    public interface ITestB
+    {
+        void Print();
+    }
+
+    public class Test : ITestA, ITestB
+    {
+        public void Print() => Console.WriteLine(nameof(Test));
+        void ITestA.Print() => Console.WriteLine(nameof(ITestA));
+        void ITestB.Print() => Console.WriteLine(nameof(ITestB));
     }
 }
- 
